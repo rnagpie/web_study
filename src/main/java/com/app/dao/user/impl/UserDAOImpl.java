0e893d802b0115c453b10c0d2f12cf925d7aa060
KKaChi <rnagpie@gmail.com>
2025-07-23 16:21:03 +0900

    계정 로그인 성공 여부 처리하는 방법 추가

--- src/main/java/com/app/dao/user/impl/UserDAOImpl.java
+++ src/main/java/com/app/dao/user/impl/UserDAOImpl.java
@@ -38,5 +38,11 @@ public class UserDAOImpl implements UserDAO {
 		
 		return user;
 	}
+
+	@Override
+	public User checkUserLogin(User user) {
+		 User loginUser = sqlSessionTemplate.selectOne("user_mapper.checkUserLogin", user);
+		return loginUser;
+	}
 	
 }
